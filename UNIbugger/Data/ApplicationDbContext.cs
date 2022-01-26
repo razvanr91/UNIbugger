63dbabd739a8bd01151954da07812140e5890192
razvanr91 <razvanroman1991@gmail.com>
2022-01-26 13:02:00 +0000

    Create BTUser class and add NpgSql packages

--- UNIbugger/Data/ApplicationDbContext.cs
+++ UNIbugger/Data/ApplicationDbContext.cs
@@ -3,10 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using UNIbugger.Models;
 
 namespace UNIbugger.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<BTUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
---
+++ UNIbugger/Models/BTUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UNIbugger.Models
+{
+    public class BTUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
+    }
+}
