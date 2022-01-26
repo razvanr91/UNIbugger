f3066a33e447fc6921f0b0106a2230e38cbcfa63
razvanr91 <razvanroman1991@gmail.com>
2022-01-26 13:03:28 +0000

    Update Startup.cs

--- UNIbugger/Startup.cs
+++ UNIbugger/Startup.cs
@@ -28,7 +28,7 @@ namespace UNIbugger
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+                options.UseNpgsql(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
