a9d40d48d8a8730f39e4e618fc50b1cc3409905b
razvanr91 <razvanroman1991@gmail.com>
2022-01-27 14:11:36 +0000

    Create TicketComment.cs

---
+++ UNIbugger/Models/TicketComment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace UNIbugger.Models
+{
+    public class TicketComment
+    {
+        public Guid Id { get; set; }
+
+        [DisplayName("Member Comment")]
+        public string Comment { get; set; }
+
+        [DisplayName("Creation Time")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Ticket")]
+        public Guid TicketId { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+        // Navigation Properties
+
+        public virtual Ticket Ticket { get; set; }
+
+        public virtual BTUser User { get; set; }
+    }
+}
