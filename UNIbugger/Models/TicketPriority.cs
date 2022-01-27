bd53370bf6a4c7441b85ac50cf8d4a3265465e3e
razvanr91 <razvanroman1991@gmail.com>
2022-01-27 11:51:46 +0000

    Create TicketPriority.cs

---
+++ UNIbugger/Models/TicketPriority.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace UNIbugger.Models
+{
+    public class TicketPriority
+    {
+        public Guid Id { get; set; }
+
+        [DisplayName("Priority Name")]
+        public string Name { get; set; }
+    }
+}
