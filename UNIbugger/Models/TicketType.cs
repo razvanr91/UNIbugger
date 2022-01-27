8eba99b69682866bf1849f575ced2b72b5df2329
razvanr91 <razvanroman1991@gmail.com>
2022-01-27 11:49:19 +0000

    Create TicketType model

---
+++ UNIbugger/Models/TicketType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace UNIbugger.Models
+{
+    public class TicketType
+    {
+        public Guid Id { get; set; }
+
+        [DisplayName("Type Name")]
+        public string Name { get; set; }
+    }
+}
