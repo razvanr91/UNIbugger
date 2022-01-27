ae1be5cdb7299b9c8a9dcec1ecbee6f8627e4473
razvanr91 <razvanroman1991@gmail.com>
2022-01-27 11:50:41 +0000

    Create TicketStatus.cs

---
+++ UNIbugger/Models/TicketStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace UNIbugger.Models
+{
+    public class TicketStatus
+    {
+        public Guid Id { get; set; }
+
+        [DisplayName("Status Name")]
+        public string Name { get; set; }
+    }
+}
