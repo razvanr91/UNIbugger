7aaaeb0f83787a8e405563d352ceba62e3b77032
razvanr91 <razvanroman1991@gmail.com>
2022-01-28 01:45:22 +0000

    Create Project and ProjectPriority classes

--- UNIbugger/Models/Project.cs
+++ UNIbugger/Models/Project.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UNIbugger.Models
 {
@@ -8,33 +11,48 @@ namespace UNIbugger.Models
     {
         public Guid Id { get; set; }
 
-        public string CompanyId { get; set; }
+        [DisplayName("Company")]
+        public string? CompanyId { get; set; }
 
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Project Name")]
         public string Name { get; set; }
 
+        [DisplayName("Description")]
         public string Description { get; set; }
 
+        [DisplayName("Start Date")]
         public DateTimeOffset StartDate { get; set; }
 
+        [DisplayName("End Date")]
         public DateTimeOffset EndDate { get; set; }
 
-        public Guid ProjectPriorityId { get; set; }
+        [DisplayName("Priority")]
+        public Guid? ProjectPriorityId { get; set; }
 
+        [NotMapped]
+        [DataType(DataType.Upload)]
         public IFormFile FormFile { get; set; }
 
+        [DisplayName("File Name")]
         public string FileName { get; set; }
 
         public byte[] FileData { get; set; }
 
+        [DisplayName("File Extension")]
         public string FileContentType { get; set; }
+
+        [DisplayName("Archived")]
+        public bool Archived { get; set; }
 
         // Navigation Properties
 
-        public virtual Company Company { get; set }
+        public virtual Company Company { get; set; }
 
         public virtual ProjectPriority ProjectPriority { get; set; }
 
-        public virtual ICollection<Member> Members { get; set; } = new HashSet<Member>();
+        public virtual ICollection<BTUser> Members { get; set; } = new HashSet<BTUser>();
 
         public virtual ICollection<Ticket> Tickets { get; set; } = new HashSet<Ticket>();
     }
