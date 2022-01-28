ab75cb03005b49bb981c20ed430f4ac9388cc890
razvanr91 <razvanroman1991@gmail.com>
2022-01-28 13:32:29 +0000

    Create Notification.cs

---
+++ UNIbugger/Models/Notification.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace UNIbugger.Models
+{
+    public class Notification
+    {
+        public Guid Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public Guid TicketId { get; set; }
+
+        [Required]
+        [DisplayName("Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayName("Message")]
+        public string Message { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Creation Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [Required]
+        [DisplayName("Recipient")]
+        public Guid RecipientId { get; set; }
+
+        [Required]
+        [DisplayName("Sender")]
+        public Guid SenderId { get; set; }
+
+        [DisplayName("Has been viewed")]
+        public bool Viewed { get; set; }
+
+        // Navigation Properties
+
+        public virtual Ticket Ticket { get; set; }
+
+        public virtual BTUser Recipient { get; set; }
+
+        public virtual BTUser Sender { get; set; }
+    }
+}
