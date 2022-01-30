f4965acbfbfcaa9c2fcdb747cb48c8c4500ec9be
razvanr91 <razvanroman1991@gmail.com>
2022-01-30 00:07:11 +0000

    Create IBTRolesService.cs

---
+++ UNIbugger/Services/Interfaces/IBTRolesService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UNIbugger.Models;
+
+namespace UNIbugger.Services.Interfaces
+{
+    public interface IBTRolesService
+    {
+        public Task<bool> IsUserInRoleAsync(BTUser user, string role);
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user);
+
+        public Task<bool> AddUserToRoleAsync(BTUser user, string role);
+
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string role);
+
+        public Task<bool> RemoveUserFromMultipleRolesAsync(BTUser user, IEnumerable<string> roles);
+
+        public Task<List<BTUser>> GetUsersInRoleAsync(string role, string companyId);
+
+        public Task<List<BTUser>> GetUsersNotInRoleAsync(string role, string companyId);
+
+        public Task<string> GetRoleNameByIdAsync(string roleId);
+    }
+}
