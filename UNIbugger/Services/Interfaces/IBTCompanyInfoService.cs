11f115a204743ad7dee3d3489c2f21b66da6f0d9
razvanr91 <razvanroman1991@gmail.com>
2022-01-31 00:22:47 +0000

    Create IBTCompanyInfoService.cs

---
+++ UNIbugger/Services/Interfaces/IBTCompanyInfoService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UNIbugger.Models;
+
+namespace UNIbugger.Services.Interfaces
+{
+    public interface IBTCompanyInfoService
+    {
+        public Task<Company> GetCompanyInfoByIdAsync(string? companyId);
+
+        public Task<List<BTUser>> GetAllMembersAsync(string? companyId);
+        
+        public Task<List<Project>> GetAllProjectsAsync(string? companyId);
+
+        public Task<List<Ticket>> GetAllTicketsAsync(string? companyId);
+    }
+}
